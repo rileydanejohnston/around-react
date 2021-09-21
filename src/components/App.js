696d987a79137113492cfe251716650cb44f2204
Riley Johnston <rileyjohnston@Rileys-iMac.local>
2021-09-21 13:51:14 -0500

    move main, header, footer to separate component files

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,14 @@
-import logo from './logo.svg';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className='root'>
       <div className='page'>
-        <header className='header'>
-          <div className='logo'></div>
-        </header>
-        <main className='main'>
-          <section className='profile'>
-            <div className='profile__avatar'>
-              <button className='profile__avatar-btn' type='button'></button>
-            </div>
-            <div className='profile__info-container'>
-              <div className='profile__info'>
-                <div className='profile__name-button'>
-                  <h1 className='profile__name'>Jacques Cousteau</h1>
-                  <button className='profile__edit-button' type='button'></button>
-                </div>
-                <p className='profile__about'></p>
-              </div>
-              <button className='profile__add-button' type='button'></button>
-            </div>
-          </section>
-          <section className='locations'>
-            <ul className='cards'>
-            </ul>
-          </section>
-        </main>
-        <footer className='footer'>
-          <p className='footer__copyright'>&copy; 2021 Around The U.S.</p>
-        </footer>
+        <Header />
+        <Main />
+        <Footer />
         <div className='popup' id='confirmPopup'>
           <div className='popup__container'>
             <button className='popup__close' type='button'></button>
---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <main className='main'>
+      <section className='profile'>
+        <div className='profile__avatar'>
+          <button className='profile__avatar-btn' type='button'></button>
+        </div>
+        <div className='profile__info-container'>
+          <div className='profile__info'>
+            <div className='profile__name-button'>
+              <h1 className='profile__name'>Jacques Cousteau</h1>
+              <button className='profile__edit-button' type='button'></button>
+            </div>
+            <p className='profile__about'></p>
+          </div>
+          <button className='profile__add-button' type='button'></button>
+        </div>
+      </section>
+      <section className='locations'>
+        <ul className='cards'>
+        </ul>
+      </section>
+    </main>
+  )
+}
