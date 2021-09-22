f0052bd9659ebdc3bac2d15f8c1c3819a17198f3
Riley Johnston <rileyjohnston@Rileys-iMac.local>
2021-09-22 18:31:02 -0500

    port api to main component and request name, about, avatar from server

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,39 @@
 import React from 'react'
+import api from '../utils/api';
 
 export default function Main(props) {
+
+  const [userName, setUserName] = React.useState('Jacques Cousteau');
+  const [userDescription, setUserDescription] = React.useState('Explorer');
+  const [userAvatar, setUserAvatar] = React.useState('../images/jacques.png');
+
+  React.useEffect(() => {
+    api.getUserInfo()
+    .then(({ name, about, avatar }) => {
+      setUserName(name);
+      setUserDescription(about);
+      setUserAvatar(avatar);
+    })
+    .catch((err) => console.log(err));
+    
+    return () => {
+      console.log('User name, about and avatar finished mounting.');
+    }
+  }, [])
 
   return (
     <main className='main'>
       <section className='profile'>
-        <div className='profile__avatar' onClick={props.onEditAvatarClick}>
+        <div className='profile__avatar' onClick={props.onEditAvatarClick} style={{ backgroundImage: `url(${userAvatar})` }}>
           <button className='profile__avatar-btn' type='button'></button>
         </div>
         <div className='profile__info-container'>
           <div className='profile__info'>
             <div className='profile__name-button'>
-              <h1 className='profile__name'>Jacques Cousteau</h1>
+              <h1 className='profile__name'>{userName}</h1>
               <button className='profile__edit-button' type='button' onClick={props.onEditProfileClick}></button>
             </div>
-            <p className='profile__about'></p>
+            <p className='profile__about'>{userDescription}</p>
           </div>
           <button className='profile__add-button' type='button' onClick={props.onAddPlaceClick}></button>
         </div>
---
+++ src/utils/api.js
@@ -0,0 +1,132 @@
+class Api {
+  constructor(settings) {
+    this._baseUrl = settings.baseUrl;
+    this._auth = settings.headers.authorization;
+    this._headers = settings.headers;
+  }
+
+  _handleResponse(res) {
+    if (res.ok){
+      return res.json();
+    }
+    return Promise.reject();
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: {
+        authorization: this._auth
+      }
+    })
+      .then((res) => {
+        return this._handleResponse(res);
+      });
+  }
+
+  getCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: {
+        authorization: this._auth
+      }
+    })
+      .then((res) => {
+        return this._handleResponse(res);
+      });
+  }
+
+  updateProfile({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: {
+        authorization: this._auth,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        about
+      })
+    })
+    .then((res) => {
+      return this._handleResponse(res);
+    })
+  }
+
+  addCard(name, link) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: {
+        authorization: this._auth,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        link
+      })
+    })
+    .then((res) => {
+      return this._handleResponse(res);
+    })
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: {
+        authorization: this._auth
+      }
+    })
+    .then((res) => {
+      return this._handleResponse(res);
+    })
+  }
+
+  addLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: "PUT",
+      headers: {
+        authorization: this._auth
+      }
+    })
+    .then((res) => {
+      return this._handleResponse(res);
+    })
+  }
+
+  removeLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: "DELETE",
+      headers: {
+        authorization: this._auth
+      }
+    })
+    .then((res) => {
+      return this._handleResponse(res);
+    })
+  }
+
+  updateProfilePic(avatar) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: {
+        authorization: this._auth,
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({
+        avatar
+      })
+    })
+    .then((res) => {
+      return this._handleResponse(res);
+    })
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://around.nomoreparties.co/v1/group-10",
+  headers: {
+      authorization: "2a32e80d-c23f-4ed9-9cf5-671c417d0702",
+      "Content-Type": "application/json"
+    }
+});
+
+export default api;
