26864945c549b330c6668d57cbaf55edfb322396
Riley Johnston <rileyjohnston@Rileys-iMac.local>
2021-09-24 17:24:15 -0500

    destructure props in card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
-export default function Card(props) {
+export default function Card({ card, onCardClick}) {
 
   function handleClick() {
-    props.onCardClick(props.card);
+    onCardClick(card);
   }
   
   return (
-    <li key={props.id} className='cards__item'>
+    <li key={card.id} className='cards__item'>
         <button className='cards__close-button' type='button' />
-        <img className='cards__photo' src={props.card.link} alt={props.card.name} onClick={handleClick} />
+        <img className='cards__photo' src={card.link} alt={card.name} onClick={handleClick} />
         <div className='cards__label'>
-          <h2 className='cards__name'>{props.card.name}</h2>
+          <h2 className='cards__name'>{card.name}</h2>
           <div className='cards__like'>
             <button className='cards__like-button' type='button' />
-            <p className='cards__like-count'>{props.card.likes.length}</p>
+            <p className='cards__like-count'>{card.likes.length}</p>
           </div>
         </div>
       </li>
