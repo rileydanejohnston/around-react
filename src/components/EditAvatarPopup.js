1701e3f8a85c5b5630bbddc23f18a6b5831c178a
Riley Johnston <rileyjohnston@Rileys-iMac.local>
2021-10-08 22:15:41 -0500

    remove handleAvatarChange/onSubmit from EditAvatarPopup, use current.value instead

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -3,22 +3,16 @@ import PopupWithForm from './PopupWithForm'
 
 function EditAvatarPopup(props) {
 
-  // I don't understand why we used refs instead of state.
-  // is it just for practice?
   const avatarRef = React.useRef('');
-
-  function handleAvatarChange(e) {
-    avatarRef.current = e.target.value;
-  }
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.onUpdateAvatar(avatarRef);
+    props.onUpdateAvatar(avatarRef.current.value);
   }
 
   return (
     <PopupWithForm title='Change profile picture' name='avatar' isOpen={props.isOpen} onSubmit={handleSubmit} onClose={props.onClose}>
-      <input className='popup__about popup__about_profile-pic popup__input' ref={avatarRef} onChange={handleAvatarChange} id='profile-pic-url' type='url' placeholder='Image link' name='pic' required />
+      <input className='popup__about popup__about_profile-pic popup__input' ref={avatarRef}  id='profile-pic-url' type='url' placeholder='Image link' name='pic' required />
       <span className='popup__error' id='profile-pic-url-error' />
     </PopupWithForm>
   )
