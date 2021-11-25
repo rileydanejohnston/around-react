08efb670c7db9240553e3079c79e61e63eeee081
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-25 16:53:32 -0600

    fix card delete button logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,6 @@ export default function Card({ card, onCardLike, onCardClick, onDeleteClick }) {
   const currentUser = React.useContext(CurrentUserContext);
 
   const isOwner = currentUser._id === card.ownerId;
-  const deleteBtnClass = (`cards__close-button ${isOwner ? 'cards__close-button_visible' : 'cards__close-button_hidden'}`);
 
   const isLiked = card.likes.some(like => like._id === currentUser._id);
   const likeBtnClass = isLiked ? 'cards__like-button cards__like-button_active' : 'cards__like-button';
@@ -25,7 +24,7 @@ export default function Card({ card, onCardLike, onCardClick, onDeleteClick }) {
   
   return (
     <li className='cards__item'>
-        <button className={deleteBtnClass} onClick={handleDeleteClick} type='button' />
+        {isOwner && <button className='cards__close-button' onClick={handleDeleteClick} type='button' />}
         <img className='cards__photo' src={card.link} alt={card.name} onClick={handleClick} />
         <div className='cards__label'>
           <h2 className='cards__name'>{card.name}</h2>
